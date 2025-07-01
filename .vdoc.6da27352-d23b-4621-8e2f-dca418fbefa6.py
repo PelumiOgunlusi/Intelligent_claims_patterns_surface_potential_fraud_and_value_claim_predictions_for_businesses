f2de45798bd43c5baf8ea233da3b5a04bf3990ce
Ogunlusi Samuel Pelumi <samuelogunlusi07@hotmail.com>
2025-07-01 11:34:47 +0100

    Delete .vdoc.6da27352-d23b-4621-8e2f-dca418fbefa6.py

--- .vdoc.6da27352-d23b-4621-8e2f-dca418fbefa6.py
+++
@@ -1,8 +0,0 @@
-# type: ignore
-# flake8: noqa
-# replacing ? symbol in specific columns with other values
-insurance['collision_type'] = insurance['collision_type'].replace('?', 'No Collision')
-insurance['property_damage'] = insurance['property_damage'].replace('?', 'Unsure')
-insurance['police_report_available'] = insurance['police_report_available'].replace('?', 'In Progress')
-
-
